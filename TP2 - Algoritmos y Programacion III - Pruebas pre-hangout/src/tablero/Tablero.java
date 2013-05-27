1248b851a7c6eb89b91e4ea6d0eb73bab167a4ca
maselloleandro@gmail.com <maselloleandro@gmail.com@ea0342d1-efbc-621f-1da0-0044ef7be2ab>
2013-05-27 17:05:37 +0000

    Se documentan metodos de la clase Tablero

--- TP2 - Algoritmos y Programacion III - Pruebas pre-hangout/src/tablero/Tablero.java
+++ TP2 - Algoritmos y Programacion III - Pruebas pre-hangout/src/tablero/Tablero.java
@@ -10,10 +10,15 @@ public class Tablero {
 	/*
 	 * Constructor de la clase. Inicializa la clase Tablero con una cantidad determinada de columnas y filas.
 	 * A su vez, inicializa el tablero colocando arbitrariamente a las naves del juego.
+	 * 
 	 * Precondiciones:
 	 * 		cantidadDeColumnas y cantidadDeFilas deben ser mayores a 0.
 	 */
 		
+	/*	if((cantidadDeColumnas<=0) & (cantidadDeFilas<=0)){
+			levantarExcepcion
+		}
+	*/	
 		columnasDelTablero = this.inicializarColumnasConFilas(cantidadDeColumnas , cantidadDeFilas);
 		this.colocarNavesEnElTablero();		
 	}
@@ -23,7 +28,13 @@ public class Tablero {
 	}
 
 	private ColeccionDeColumnas inicializarColumnasConFilas(int cantidadDeColumnas, int cantidadDePosiciones) {
-		
+	/*
+	 * Metodo que crea una instancia de ColeccionDeColumnas, con una cantidad de Columnas de acuerdo a lo estipulado por parametro
+	 * En donde cada columna va a contener una cantidad de posiciones estipulada por parametro.
+	 * 
+	 * Precondiciones:	cantidadDeColumnas y cantidadDePosiciones deben ser mayores a 0.
+	 * Postcondiciones: devuelve una instancia de ColeccionDeColumnas
+	 */
 		ColeccionDeColumnas unaColeccion = new ColeccionDeColumnas();
 		
 		for(int numeroDeColumnaActual=1 ; numeroDeColumnaActual<=cantidadDeColumnas ; numeroDeColumnaActual++){
@@ -39,7 +50,14 @@ public class Tablero {
 	}
 
 	public Posicion obtenerPosicion(char columnaBuscada , int filaBuscada){
-		
+	/*
+	 * Metodo que tiene como objetivo obtener una posicion deseada del tablero.
+	 * 
+	 *  Precondiciones: 
+	 *  	columnaBuscada debe corresponder a una columna valida del tablero.
+	 *  	filaBuscada debe corresponder a una fila valida del tablero.
+	 *  Postcondiciones: devuelve la una instancia de Posicion, de acuerdo a los parametros estipulados por parametro.
+	 */
 		Columna columnaDeLaPosicionRequerida = columnasDelTablero.obtenerLaColumnaDeIndice(columnaBuscada);
 		
 		Posicion posicionRequerida = columnaDeLaPosicionRequerida.obtenerPosicionDeFila(filaBuscada);
@@ -48,7 +66,7 @@ public class Tablero {
 	}
 		
 	public int cantidadDeColumnas() {
-		
+
 		return columnasDelTablero.cantidadDeColumnas();
 		
 	}
