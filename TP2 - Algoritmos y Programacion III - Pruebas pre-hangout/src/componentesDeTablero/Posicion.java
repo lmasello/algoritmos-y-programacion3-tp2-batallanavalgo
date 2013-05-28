ce17b8f0c6e43c84b2d35cbb15e5ce82bdaf1b0c
facundo.caldora <facundo.caldora@ea0342d1-efbc-621f-1da0-0044ef7be2ab>
2013-05-28 03:24:08 +0000

    Pequeña implementacion de la clase Posicion que tiene Componentes de naves y Disparos

--- TP2 - Algoritmos y Programacion III - Pruebas pre-hangout/src/componentesDeTablero/Posicion.java
+++ TP2 - Algoritmos y Programacion III - Pruebas pre-hangout/src/componentesDeTablero/Posicion.java
@@ -1,5 +1,25 @@
 package componentesDeTablero;
 
-public class Posicion {
+import colecciones.ColeccionDeComponentes;
+import colecciones.ColeccionDeDisparos;
 
+public class Posicion {
+	
+	ColeccionDeComponentes componentesDeNaveEnPosicion;
+	ColeccionDeDisparos disparosEnPosicion;
+	
+	public ColeccionDeComponentes obtenerComoponentesEnPosicion(){
+	/* Devuelve la lista de componentes de naves en la posicion */
+		
+		return componentesDeNaveEnPosicion;
+		
+	}
+	
+	public ColeccionDeDisparos obtenerDisparosEnPosicion(){
+	/* Devuelve la lista de disparos en la posicion */
+		
+		return disparosEnPosicion;
+		
+	}
+	
 }
