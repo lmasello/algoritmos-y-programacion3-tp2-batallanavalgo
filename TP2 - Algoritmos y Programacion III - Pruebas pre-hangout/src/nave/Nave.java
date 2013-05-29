401cca82ce36d373a2b096dbd5502e75a922d416
maselloleandro@gmail.com <maselloleandro@gmail.com@ea0342d1-efbc-621f-1da0-0044ef7be2ab>
2013-05-29 04:09:36 +0000

    Se agregan dos metodos a la clase Nave para lograr la validacion de la asignacion de las posiciones en el constructor del Tablero.
    Dichos metodos son:
    numeroDeComponenteDeLaProa. El cual basicamente toma como primer componente a la proa --> devuelve 1 (metodo trivial)
    
    obtenerComponente(indice) El cual devuelve una componente de la nave

--- TP2 - Algoritmos y Programacion III - Pruebas pre-hangout/src/nave/Nave.java
+++ TP2 - Algoritmos y Programacion III - Pruebas pre-hangout/src/nave/Nave.java
@@ -46,4 +46,29 @@ public abstract class Nave {
 		estaDestruida = true;
 	}
 
+	public int numeroDeComponenteDeLaProa(){
+	/*
+	 * Se toma como supuesto que la proa es la primer componente de la nave	
+	 */
+		return 1;
+	}
+
+	public ComponenteDeNave obtenerComponenteDeNumero(int numeroDeComponenteDeseada) {
+	/*
+	 * Devuelve la componente de la nave de acuerdo al indice pasado por parametro.
+	 * 
+	 * Precondiciones: 
+	 * 		numeroDeComponenteDeseada debe ser mayor a 0 y menor o igual a la cantidad de componentes de la nave	
+	 */
+		
+		/*if((numeroDeComponenteDeseada<=0)&(numeroDeComponenteDeseada>this.cantidadDeComponentes())){
+			tirar error
+		}*/
+			
+		return componentes.obtenerComponente(numeroDeComponenteDeseada-1);
+		
+	}
+	
+	
+	
 }
