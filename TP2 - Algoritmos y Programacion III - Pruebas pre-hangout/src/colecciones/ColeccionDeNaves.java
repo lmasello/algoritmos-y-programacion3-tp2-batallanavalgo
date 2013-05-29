63a1c52c4f27f2e29fd2135da1d0d34ee61294e1
maselloleandro@gmail.com <maselloleandro@gmail.com@ea0342d1-efbc-621f-1da0-0044ef7be2ab>
2013-05-29 04:05:03 +0000

    Se corrige la clase ColeccionDeNaves agregando los imports de las naves correspondientes

--- TP2 - Algoritmos y Programacion III - Pruebas pre-hangout/src/colecciones/ColeccionDeNaves.java
+++ TP2 - Algoritmos y Programacion III - Pruebas pre-hangout/src/colecciones/ColeccionDeNaves.java
@@ -2,35 +2,82 @@ package colecciones;
 
 import java.util.ArrayList;
 
+import nave.Buque;
+import nave.Destructor;
+import nave.Lancha;
 import nave.Nave;
-
+import nave.PortaAviones;
+import nave.RompeHielos;
 
 public class ColeccionDeNaves {
-	
-	ArrayList<Nave> naves;
+
+	ArrayList<Nave> coleccion;
 	
 	public ColeccionDeNaves(){
 		
-		naves = new ArrayList<Nave>();
-		
+		coleccion = new ArrayList<Nave>();
+
 	}
 	
 	public void agregarNave(Nave naveAAgregar){
-		
-		naves.add(naveAAgregar);
-		
+		coleccion.add(naveAAgregar);
 	}
-	
-	public Nave obtenerNave(int indiceNave){
-		
-		/* si no es valido el indice lanzar excepcion*/
-		return naves.get(indiceNave - 1);
-	}
-	
-	public int cantidadDeNaves(){
-		
-		return naves.size();
-	
-	}
-}
 
+	public void establecerNavesDelJuego() {
+	/*
+	 * Agrega las naves establecidas por la consigna del trabajo a la coleccion.	
+	 */
+		this.agregarUnaCantidadDeVecesNaveLancha(2);
+		this.agregarUnaCantidadDeVecesNaveDestructor(2);
+		
+		this.agregarBuque();
+		this.agregarPortaAviones();
+		this.agregarRompeHielos();
+		
+	}
+
+	private void agregarRompeHielos() {
+		Nave naveAAgregar = new RompeHielos(); 
+		this.agregarNave(naveAAgregar);	
+	}
+
+	private void agregarPortaAviones() {
+		Nave naveAAgregar = new PortaAviones(); 
+		this.agregarNave(naveAAgregar);	
+	}
+
+	private void agregarBuque() {
+		Nave naveAAgregar = new Buque(); 
+		this.agregarNave(naveAAgregar);	
+	}
+
+	private void agregarUnaCantidadDeVecesNaveDestructor(int cantidadDeNaves) {
+		for(int numeroDeNaveActual = 0 ; numeroDeNaveActual < cantidadDeNaves ; numeroDeNaveActual++){
+			
+			Nave naveAAgregar = new Destructor(); 
+			this.agregarNave(naveAAgregar);
+		}
+	}
+
+	private void agregarUnaCantidadDeVecesNaveLancha(int cantidadDeNaves) {
+		
+		for(int numeroDeNaveActual = 0 ; numeroDeNaveActual < cantidadDeNaves ; numeroDeNaveActual++){
+			
+			Nave naveAAgregar = new Lancha(); 
+			this.agregarNave(naveAAgregar);
+		}
+	}
+
+	public int cantidadDeNaves() {
+		return coleccion.size();
+	}
+
+	public Nave naveDeLaPosicion(int numeroDeNaveActual) {
+		/*
+		 * Precondiciones: numeroDeNaveActual debe ser mayor o igual a 1
+		 */
+		return coleccion.get(numeroDeNaveActual-1);
+		
+	}
+	
+}
