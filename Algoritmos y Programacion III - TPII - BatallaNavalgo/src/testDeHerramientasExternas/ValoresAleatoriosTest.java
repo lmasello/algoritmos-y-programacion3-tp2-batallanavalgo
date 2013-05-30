fcc83a17d9345cb0b69692b32c1da71024e730d8
maselloleandro@gmail.com <maselloleandro@gmail.com@ea0342d1-efbc-621f-1da0-0044ef7be2ab>
2013-05-30 16:28:35 +0000

    prueba de valores aleatorios

---
+++ Algoritmos y Programacion III - TPII - BatallaNavalgo/src/testDeHerramientasExternas/ValoresAleatoriosTest.java
@@ -0,0 +1,40 @@
+package testDeHerramientasExternas;
+
+import static org.junit.Assert.*;
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+import componentesDeTablero.Tablero;
+import excepciones.ValorDeParametroFueraDeRango;
+import excepciones.ValoresDeParametroFueraDeRango;
+
+public class ValoresAleatoriosTest extends TestCase{
+
+	public void testGenerarValorAleatorioEntreDosValores() throws ValorDeParametroFueraDeRango, ValoresDeParametroFueraDeRango{
+		
+		Tablero tableroAProbar = new Tablero(10,10);
+		
+		for(int contador=0;contador<10;contador++){
+			
+			int numeroAleatorio = tableroAProbar.generarNumeroAleatorioEntreDosValores(1, tableroAProbar.cantidadDeColumnas());
+			System.out.println(numeroAleatorio);
+
+			assertTrue(numeroAleatorio>0);
+			assertTrue(numeroAleatorio<=tableroAProbar.cantidadDeColumnas());
+		}
+	}
+	public void testGenerarValorAleatorioEntre0Y1() throws ValorDeParametroFueraDeRango, ValoresDeParametroFueraDeRango{
+		
+		Tablero tableroAProbar = new Tablero(10,10);
+		
+		for(int contador=0;contador<10;contador++){
+			
+			int numeroAleatorio = tableroAProbar.generarNumeroAleatorioEntreDosValores(0, 1);
+			System.out.println(numeroAleatorio);
+
+			assertTrue(numeroAleatorio>=0);
+			assertTrue(numeroAleatorio<=1);
+		}
+	}
+}
