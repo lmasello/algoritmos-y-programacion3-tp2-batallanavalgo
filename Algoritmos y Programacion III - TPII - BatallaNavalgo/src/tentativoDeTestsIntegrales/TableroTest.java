5f3d9a7228a81fd86ea3edad530cbd16095a030b
maselloleandro@gmail.com <maselloleandro@gmail.com@ea0342d1-efbc-621f-1da0-0044ef7be2ab>
2013-06-03 03:11:06 +0000

    Se agregan pruebas integrales de los disparos con sus consecuencias en naves

--- Algoritmos y Programacion III - TPII - BatallaNavalgo/src/tentativoDeTestsIntegrales/TableroTest.java
+++ Algoritmos y Programacion III - TPII - BatallaNavalgo/src/tentativoDeTestsIntegrales/TableroTest.java
@@ -4,60 +4,141 @@ import static org.junit.Assert.*;
 import junit.framework.TestCase;
 
 import nave.Buque;
+import nave.Destructor;
 import nave.Lancha;
+import nave.Nave;
+import nave.RompeHielos;
 
 import org.junit.Test;
 
 import componentesDeTablero.Posicion;
 import componentesDeTablero.Tablero;
+import disparos.Disparo;
+import disparos.DisparoConvencional;
+import disparos.MinaSubmarinaDoble;
+import disparos.MinaSubmarinaPorContacto;
+import disparos.MinaSubmarinaPuntual;
+import disparos.MinaSubmarinaTriple;
+import excepciones.LargoDeNaveIncorrecto;
 import excepciones.ValorDeParametroFueraDeRango;
 import excepciones.ValoresDeParametroFueraDeRango;
 
 
 public class TableroTest extends TestCase {
 	
-	/*public void testRealizarDisparoAUnaPosicionQueTieneUnaComponenteDeResistenciaDeUnTiro() {
+	public void testRealizarDisparoAUnaNaveDeResistenciaDeUnTiro() throws ValorDeParametroFueraDeRango, ValoresDeParametroFueraDeRango, LargoDeNaveIncorrecto {
 
 		Tablero tableroDelJuego = new Tablero(10, 10);
-		Disparo disparoAgresivo = new Disparo();
+		Posicion posicionDeProaDeNaveAProbar = tableroDelJuego.obtenerPosicion('D', 4);
+		Posicion posicionDeCuerpoDeNave = tableroDelJuego.obtenerPosicion('C', 4);
+		Nave naveAProbar = new Lancha();
+		
+		tableroDelJuego.colocarComponentesEnDireccionHorizontal(naveAProbar, posicionDeProaDeNaveAProbar);
+		
+		assertEquals(false, naveAProbar.estaDestruida());
+		
+		Disparo disparoARealizar = new DisparoConvencional();
 
-		assertEquals(true, tablero.laPosicionTieneComponenteDeNave('A', 1));
-
-		tablero.realizarDisparoALaPosicion(disparoAgresivo, 'A', 1); // Al
-																		// recibir
-																		// el
-																		// impacto
-																		// la
-																		// componente
-																		// desaparece
-
-		assertEquals(false, tablero.laPosicionTieneComponenteDeNave('A', 1));
-
+		posicionDeProaDeNaveAProbar.agregarDisparoAPosicion(disparoARealizar);
+		posicionDeCuerpoDeNave.agregarDisparoAPosicion(disparoARealizar);
+		
+		tableroDelJuego.impactarDisparos();
+		assertEquals(true, naveAProbar.estaDestruida());
+		
 	}
 
-	public void testRealizarDisparoAUnaPosicionQueTieneUnaComponenteDeResistenciaDeDosTiros() {
+	public void testRealizarDisparoAUnaNaveDeResistenciaDeDosTiros() throws ValorDeParametroFueraDeRango, ValoresDeParametroFueraDeRango, LargoDeNaveIncorrecto {
 
 		Tablero tableroDelJuego = new Tablero(10, 10);
-		Disparo disparoAgresivo = new Disparo();
+		Posicion posicionDeProaDeNaveAProbar = tableroDelJuego.obtenerPosicion('D', 4);
+		Posicion posicionDeCuerpoDeNave = tableroDelJuego.obtenerPosicion('C', 4);
+		Posicion posicionDePopaDeNave = tableroDelJuego.obtenerPosicion('B', 4);
+		Nave naveAProbar = new RompeHielos();
+		
+		tableroDelJuego.colocarComponentesEnDireccionHorizontal(naveAProbar, posicionDeProaDeNaveAProbar);
+		
+		assertEquals(false, naveAProbar.estaDestruida());
+		
+		Disparo disparoARealizar = new DisparoConvencional();
 
-		assertEquals(true, tablero.laPosicionTieneComponenteDeNave('A', 1));
-
-		tablero.realizarDisparoALaPosicion(disparoAgresivo, 'A', 1);
-
-		assertEquals(true, tablero.laPosicionTieneComponenteDeNave('A', 1));
-
-		tablero.realizarDisparoALaPosicion(disparoAgresivo, 'A', 1);
-
-		assertEquals(false, tablero.laPosicionTieneComponenteDeNave('A', 1)); // Luego
-																				// de
-																				// disparar
-																				// dos
-																				// veces,
-																				// la
-																				// componente
-																				// desaparece
+		posicionDeProaDeNaveAProbar.agregarDisparoAPosicion(disparoARealizar);
+		posicionDeCuerpoDeNave.agregarDisparoAPosicion(disparoARealizar);
+		posicionDePopaDeNave.agregarDisparoAPosicion(disparoARealizar);
+		
+		tableroDelJuego.impactarDisparos();
+		assertEquals(true, naveAProbar.estaDestruida());
+		
 	}
 
+	public void testRealizarDisparoAUnaNaveQueAlRecibirUnImpactoSeDestruye() throws ValorDeParametroFueraDeRango, ValoresDeParametroFueraDeRango, LargoDeNaveIncorrecto {
 
-*/
+		Tablero tableroDelJuego = new Tablero(10, 10);
+		Posicion posicionDeProaDeNaveAProbar = tableroDelJuego.obtenerPosicion('D', 4);
+		Posicion posicionDeCuerpoDeNave = tableroDelJuego.obtenerPosicion('C', 4);
+		Nave naveAProbar = new Buque();
+		
+		tableroDelJuego.colocarComponentesEnDireccionHorizontal(naveAProbar, posicionDeProaDeNaveAProbar);
+		
+		assertEquals(false, naveAProbar.estaDestruida());
+		
+		Disparo disparoARealizar = new DisparoConvencional();
+
+		posicionDeCuerpoDeNave.agregarDisparoAPosicion(disparoARealizar);
+
+		tableroDelJuego.impactarDisparos();
+		assertEquals(true, naveAProbar.estaDestruida());
+		
+	}
+	
+	public void testRealizarDisparoAUnaNaveQueSoloRecibeDanioDeUnDisparoConvencional() throws ValorDeParametroFueraDeRango, ValoresDeParametroFueraDeRango, LargoDeNaveIncorrecto {
+
+		Tablero tableroDelJuego = new Tablero(10, 10);
+		Posicion posicionDeProaDeNaveAProbar = tableroDelJuego.obtenerPosicion('D', 4);
+		Posicion posicionDeCuerpoDeNave = tableroDelJuego.obtenerPosicion('C', 4);
+		Posicion posicionDePopaDeNave = tableroDelJuego.obtenerPosicion('B', 4);
+		
+		Nave naveAProbar = new Destructor();
+		
+		tableroDelJuego.colocarComponentesEnDireccionHorizontal(naveAProbar, posicionDeProaDeNaveAProbar);
+		
+		assertEquals(false, naveAProbar.estaDestruida());
+
+ 		Disparo disparoARealizar1 = new DisparoConvencional();
+
+ 		posicionDeProaDeNaveAProbar.agregarDisparoAPosicion(disparoARealizar1);
+		posicionDeCuerpoDeNave.agregarDisparoAPosicion(disparoARealizar1);
+		posicionDePopaDeNave.agregarDisparoAPosicion(disparoARealizar1);
+
+		tableroDelJuego.impactarDisparos();
+		assertEquals(true, naveAProbar.estaDestruida());
+		
+	}
+
+	public void testNoSeDaniaUnaNaveQueSoloRecibeDanioDeUnDisparoConvencionalYSeDisparaConOtroDisparo() throws ValorDeParametroFueraDeRango, ValoresDeParametroFueraDeRango, LargoDeNaveIncorrecto {
+
+		Tablero tableroDelJuego = new Tablero(10, 10);
+		Posicion posicionDeProaDeNaveAProbar = tableroDelJuego.obtenerPosicion('D', 4);
+		Posicion posicionDeCuerpoDeNave = tableroDelJuego.obtenerPosicion('C', 4);
+		Posicion posicionDePopaDeNave = tableroDelJuego.obtenerPosicion('B', 4);
+		
+		Nave naveAProbar = new Destructor();
+		
+		tableroDelJuego.colocarComponentesEnDireccionHorizontal(naveAProbar, posicionDeProaDeNaveAProbar);
+		
+		assertEquals(false, naveAProbar.estaDestruida());
+		
+		Disparo disparoARealizar1 = new MinaSubmarinaPuntual();
+		Disparo disparoARealizar2 = new MinaSubmarinaDoble();
+		Disparo disparoARealizar3 = new MinaSubmarinaTriple();
+		Disparo disparoARealizar4 = new MinaSubmarinaPorContacto();
+		
+ 		posicionDeProaDeNaveAProbar.agregarDisparoAPosicion(disparoARealizar1);
+		posicionDeCuerpoDeNave.agregarDisparoAPosicion(disparoARealizar2);
+		posicionDePopaDeNave.agregarDisparoAPosicion(disparoARealizar3);
+		posicionDePopaDeNave.agregarDisparoAPosicion(disparoARealizar4);
+		
+		tableroDelJuego.impactarDisparos();
+		assertEquals(false, naveAProbar.estaDestruida());
+		
+	}
 }
