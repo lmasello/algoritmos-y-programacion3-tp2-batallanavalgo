a6d3acbf8af4caa1e559eb70b42d4b5645a2cd8a
facundo.caldora <facundo.caldora@ea0342d1-efbc-621f-1da0-0044ef7be2ab>
2013-06-04 14:46:22 +0000

    Modificacion del test para que pase.

--- Algoritmos y Programacion III - TPII - BatallaNavalgo/src/tentativoDeTestsIntegrales/DisparosTest.java
+++ Algoritmos y Programacion III - TPII - BatallaNavalgo/src/tentativoDeTestsIntegrales/DisparosTest.java
@@ -1,6 +1,7 @@
 package tentativoDeTestsIntegrales;
 
 import static org.junit.Assert.*;
+import jugador.Jugador;
 import junit.framework.TestCase;
 
 import nave.Lancha;
@@ -8,7 +9,10 @@ import nave.Nave;
 
 import org.junit.Test;
 
+import colecciones.ColeccionDePosiciones;
+
 import componentesDeTablero.Posicion;
+import componentesDeTablero.RecolectorDePosicionesDeDisparo;
 import componentesDeTablero.Tablero;
 
 import disparos.Disparo;
@@ -26,15 +30,21 @@ public class DisparosTest extends TestCase{
 		Posicion posicionDeProaDeNaveAProbar = tableroDelJuego.obtenerPosicion('D', 4);
 		Posicion posicionDeCuerpoDeNave = tableroDelJuego.obtenerPosicion('C', 4);
 		Nave naveAProbar = new Lancha();
-	
+		Jugador jugador1 = new Jugador("Juan");
+		
 		tableroDelJuego.colocarComponentesEnDireccionHorizontal(naveAProbar, posicionDeProaDeNaveAProbar);
 
+		RecolectorDePosicionesDeDisparo recolector = new RecolectorDePosicionesDeDisparo(tableroDelJuego);
 		Disparo disparoARealizar = new DisparoConvencional();
 		Disparo otroDisparo = new MinaSubmarinaDoble();
 
+		ColeccionDePosiciones posicionesDelDisparoARealizar = recolector.obtenerPosicionesDeDisparo(posicionDeProaDeNaveAProbar, disparoARealizar); 
+		ColeccionDePosiciones posicionesDelOtroDisparo = recolector.obtenerPosicionesDeDisparo(posicionDeCuerpoDeNave, otroDisparo);
 		
-		posicionDeProaDeNaveAProbar.agregarDisparoAPosicion(disparoARealizar);
-		posicionDeCuerpoDeNave.agregarDisparoAPosicion(otroDisparo);
+		jugador1.asignarTablero(tableroDelJuego);
+		
+		jugador1.realizarDisparoALasPosiciones(disparoARealizar, posicionesDelDisparoARealizar);
+		jugador1.realizarDisparoALasPosiciones(otroDisparo, posicionesDelOtroDisparo);
 		
 		assertEquals(2 , posicionDeProaDeNaveAProbar.obtenerDisparosEnPosicion().cantidadDeDisparos());
 		assertEquals(1 , posicionDeCuerpoDeNave.obtenerDisparosEnPosicion().cantidadDeDisparos());
