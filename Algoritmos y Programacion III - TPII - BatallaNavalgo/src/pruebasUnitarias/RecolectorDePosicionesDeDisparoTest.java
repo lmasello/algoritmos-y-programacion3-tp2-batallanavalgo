607b686f6475e84284d8f9cad39652276454cacd
augusto.tirotta@gmail.com <augusto.tirotta@gmail.com@ea0342d1-efbc-621f-1da0-0044ef7be2ab>
2013-06-04 17:16:50 +0000

    Agrego los test's faltantes del RecolectorDePosicionesDeDisparo

--- Algoritmos y Programacion III - TPII - BatallaNavalgo/src/pruebasUnitarias/RecolectorDePosicionesDeDisparoTest.java
+++ Algoritmos y Programacion III - TPII - BatallaNavalgo/src/pruebasUnitarias/RecolectorDePosicionesDeDisparoTest.java
@@ -39,6 +39,10 @@ public class RecolectorDePosicionesDeDisparoTest extends TestCase {
 		coleccionDePosicionesADisparar = recolector.obtenerPosicionesDeDisparo(posicionADisparar, minaDoble);
 		
 		assertEquals( 4 , coleccionDePosicionesADisparar.cantidadDePosiciones() );
+		assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('A', 1)));
+		assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('A', 2)));
+		assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('B', 1)));
+		assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('B', 2)));
 		
 
 	}
@@ -54,6 +58,16 @@ public class RecolectorDePosicionesDeDisparoTest extends TestCase {
 			coleccionDePosicionesADisparar = recolector.obtenerPosicionesDeDisparo(posicionADisparar, minaTriple);
 			
 			assertEquals( 9 , coleccionDePosicionesADisparar.cantidadDePosiciones() );
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('A', 1)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('A', 2)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('A', 3)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('B', 1)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('B', 2)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('B', 3)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('C', 1)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('C', 2)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('C', 3)));
+			
 			
 	
 	}
@@ -84,6 +98,10 @@ public class RecolectorDePosicionesDeDisparoTest extends TestCase {
 		coleccionDePosicionesADisparar = recolector.obtenerPosicionesDeDisparo(posicionADisparar, minaDoble);
 		
 		assertEquals( 4 , coleccionDePosicionesADisparar.cantidadDePosiciones() );
+		assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('A', 10)));
+		assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('A', 9)));
+		assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('B', 10)));
+		assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('B', 9)));
 		
 	
 	}
@@ -99,6 +117,15 @@ public class RecolectorDePosicionesDeDisparoTest extends TestCase {
 			coleccionDePosicionesADisparar = recolector.obtenerPosicionesDeDisparo(posicionADisparar, minaTriple);
 			
 			assertEquals( 9 , coleccionDePosicionesADisparar.cantidadDePosiciones() );
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('A', 10)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('A', 9)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('A', 8)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('B', 10)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('B', 9)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('B', 8)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('C', 10)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('C', 9)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('C', 8)));
 			
 	
 	}
@@ -130,6 +157,10 @@ public class RecolectorDePosicionesDeDisparoTest extends TestCase {
 		coleccionDePosicionesADisparar = recolector.obtenerPosicionesDeDisparo(posicionADisparar, minaDoble);
 		
 		assertEquals( 4 , coleccionDePosicionesADisparar.cantidadDePosiciones() );
+		assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('J', 1)));
+		assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('J', 2)));
+		assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('I', 1)));
+		assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('I', 2)));
 		
 	
 	}
@@ -145,6 +176,15 @@ public class RecolectorDePosicionesDeDisparoTest extends TestCase {
 			coleccionDePosicionesADisparar = recolector.obtenerPosicionesDeDisparo(posicionADisparar, minaTriple);
 			
 			assertEquals( 9 , coleccionDePosicionesADisparar.cantidadDePosiciones() );
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('J', 1)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('J', 2)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('J', 3)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('I', 1)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('I', 2)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('I', 3)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('H', 1)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('H', 2)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('H', 3)));
 			
 	
 	}
@@ -175,6 +215,10 @@ public class RecolectorDePosicionesDeDisparoTest extends TestCase {
 		coleccionDePosicionesADisparar = recolector.obtenerPosicionesDeDisparo(posicionADisparar, minaDoble);
 		
 		assertEquals( 4 , coleccionDePosicionesADisparar.cantidadDePosiciones() );
+		assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('J', 10)));
+		assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('J', 9)));
+		assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('I', 10)));
+		assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('I', 9)));
 		
 	
 	}
@@ -190,6 +234,15 @@ public class RecolectorDePosicionesDeDisparoTest extends TestCase {
 			coleccionDePosicionesADisparar = recolector.obtenerPosicionesDeDisparo(posicionADisparar, minaTriple);
 			
 			assertEquals( 9 , coleccionDePosicionesADisparar.cantidadDePosiciones() );
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('J', 10)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('J', 9)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('J', 8)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('I', 10)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('I', 9)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('I', 8)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('H', 10)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('H', 9)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('H', 8)));
 			
 	
 	}
@@ -221,6 +274,15 @@ public class RecolectorDePosicionesDeDisparoTest extends TestCase {
 			coleccionDePosicionesADisparar = recolector.obtenerPosicionesDeDisparo(posicionADisparar, minaDoble);
 			
 			assertEquals( 9 , coleccionDePosicionesADisparar.cantidadDePosiciones() );
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('H', 7)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('H', 8)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('H', 6)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('I', 7)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('I', 8)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('I', 6)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('G', 7)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('G', 8)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('G', 6)));
 			
 	}
 	
@@ -235,6 +297,33 @@ public class RecolectorDePosicionesDeDisparoTest extends TestCase {
 			coleccionDePosicionesADisparar = recolector.obtenerPosicionesDeDisparo(posicionADisparar, minaTriple);
 			
 			assertEquals( 25 , coleccionDePosicionesADisparar.cantidadDePosiciones() );
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('E', 3)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('E', 4)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('E', 5)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('E', 6)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('E', 7)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('D', 3)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('D', 4)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('D', 5)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('D', 6)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('D', 7)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('C', 3)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('C', 4)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('C', 5)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('C', 6)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('C', 7)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('F', 3)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('F', 4)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('F', 5)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('F', 6)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('F', 7)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('G', 3)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('G', 4)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('G', 5)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('G', 6)));
+			assertEquals(true, coleccionDePosicionesADisparar.seEncuentraLaPosicion(tablero.obtenerPosicion('G', 7)));
+		
+			
 			
 	}
 
